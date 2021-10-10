43db172c5ecc906009770e915b7c9a11f17b4808
Michal Pawel Wadowski <80638034+Michau94@users.noreply.github.com>
2021-10-10 10:59:58 +0200

    added header Routes

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'HomeController@index');
+Route::get('/', 'HomeController@index')->name('home');
+
+Route::get('/trips', 'HomeController@trips')->name('trips');
